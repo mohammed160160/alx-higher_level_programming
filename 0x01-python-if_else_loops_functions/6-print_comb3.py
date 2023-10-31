3e6d04c1af31dc0425ee62930b53516ab9dd901c
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-31 10:33:49 +0000

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-    for j in range(0,10):
+    for j in range(0, 10):
         if i != j and i < j:
-            print("{:d}{:d}".format(i,j), end="")
+            print("{:d}{:d}".format(i, j), end="")
             print(end="\n" if (i*10 + j) == 89 else ", ")
