ee2cbad527fa7e2455e7590ec9a0467b591053d1
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-31 09:41:12 +0000

    task 9

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 100):
+    print("{:02}".format(i), end="")
     if i != 99:
-        print(f"{i:02}", end=", ")
+        print(", ", end="")
     else:
-        print(i)
+        print()
