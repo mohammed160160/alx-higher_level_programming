59b195cf28d287a9ea0644d21584ea27dc0de492
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-31 12:39:46 +0000

    task 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     length = len(str)
-    if n > (length - 1) and n < 0:
+    if n > (length - 1) or n < 0:
         print(str)
     else:
         removed = str[0:n] + str[-n:length]
