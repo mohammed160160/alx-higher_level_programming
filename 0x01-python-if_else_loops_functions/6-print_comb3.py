9dc638627d643854610f15d470ded31adbab2345
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-31 08:58:18 +0000

    tasks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-# YOUR CODE HERE
+for i in range(0, 10):
+    for j in range(0,10):
+        if i != j and i < j:
+            print(f"{i}{j}", end="")
+            if ((i * 10) + j) != 89:
+                print(f", ", end="")
+            else:
+                print()
