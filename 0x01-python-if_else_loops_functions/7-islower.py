5730806d72a2ab8678cab341c6fac3b1f11edce9
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-31 10:48:37 +0000

    task8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
+    value = ord(c)
+    if value > 96 or value < 122:
+        return (True)
+    else:
+        return (False)
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
+    value = ord(c)
+    if value > 64 or value < 91:
+        return (True)
+    else:
+        return (False)
