83b72749a7c68a014384754cbdcfdbad79ab5fe4
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-31 12:02:45 +0000

    tasks7 and 8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     value = ord(c)
-    if value > 96 and value < 122:
+    if value > 96 and value < 123:
         return (True)
     else:
         return (False)
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     for i in range(0, length):
         cl = str[i]
         value = ord(cl)
-        if value > 96 and value < 122:
+        if value > 96 and value < 123:
             value = value - 32
         print("{:c}".format(value), end="")
     print(end="\n")
