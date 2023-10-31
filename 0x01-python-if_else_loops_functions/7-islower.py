eadeaa3972f5135d0b8e20946bd52cdf60c16d6d
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-31 11:29:01 +0000

    task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     value = ord(c)
-    if value > 96 or value < 122:
+    if value > 96 and value < 122:
         return (True)
     else:
         return (False)
