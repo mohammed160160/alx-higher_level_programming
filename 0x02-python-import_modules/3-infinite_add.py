794258da636bf9a532d83cd9e29a7dd7fbc1a369
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-02 11:47:01 +0000

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    count = len(sys.argv) - 1
+    total = 0
+    for i in range(0, count):
+        j = i + 1
+        total += int(sys.argv[j])
+    print("{:d}".format(total))
