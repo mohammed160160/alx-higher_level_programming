4e31923eab2293faaff745808b4d7a8fd1942f9e
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-02 12:18:53 +0000

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import importlib
+    printing = importlib.import_module('hidden_4')
+    number = dir(printing)
+    length = len(number)
+    for i in range(0, length):
+        if not number[i].startswith("__"):
+            print(number[i])
