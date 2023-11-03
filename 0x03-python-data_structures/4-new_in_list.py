daeb55d6694c7820cdf1666c8e18b86690cfb479
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-03 11:03:39 +0000

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
+    if idx < 0 or idx > (length - 1):
+        return (my_list)
+    new_list = list(my_list)
+    new_list[idx] = element
+    return (new_list)
