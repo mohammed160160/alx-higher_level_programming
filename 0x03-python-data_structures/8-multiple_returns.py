b6bce3d65878979c1e40b489ef0e2b4403d6f548
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-03 11:17:20 +0000

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        firstletter = None
+    else:
+        firstletter = sentence[0]
+    return (length, firstletter)
