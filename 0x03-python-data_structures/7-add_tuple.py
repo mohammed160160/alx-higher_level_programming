faab0b88924fe267f251c7d4e6660929bf53e6a9
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-04 10:43:07 +0000

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    first_element = tuple_a[0] + tuple_b[0]
-    second_element = tuple_a[1] + tuple_b[1]
+    alength = len(tuple_a)
+    blength = len(tuple_b)
+    if alength == 0:
+        firsta = 0
+    else:
+        firsta = tuple_a[0]
+
+    if (alength == 0 or alength == 1):
+        seconda = 0
+    else:
+        seconda = tuple_a[1]
+
+    if blength == 0:
+        firstb = 0
+    else:
+        firstb = tuple_b[0]
+        
+    if (blength == 0 or blength == 1):
+        secondb = 0
+    else:
+        secondb = tuple_b[1]
+
+    first_element = firsta + firstb
+    second_element = seconda + secondb
     tuple_elements = (first_element, second_element)
     return (tuple_elements)
