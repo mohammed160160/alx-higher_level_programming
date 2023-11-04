df302000765cad1085805ff642414b5d9c23a01e
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-04 15:49:25 +0000

    task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     length = len(my_list)
-    for i in range (0, length):
+    for i in range(0, length):
         print("{:d}".format(my_list[i]))
