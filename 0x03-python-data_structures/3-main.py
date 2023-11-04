f8dc3694640adc0d415b5efccf7ab222958cbc35
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-04 15:50:53 +0000

    task 3

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -3,3 +3,6 @@ print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_
 
 my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
+print("-------------")
+new_list = []
+print_reversed_list_integer(new_list)
