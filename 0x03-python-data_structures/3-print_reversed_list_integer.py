372825a53c628270b1a674626f550e7e9de56361
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-05 14:21:11 +0000

    tasks

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return None
     my_list.reverse()
     length = len(my_list)
     for i in range(0, length):
