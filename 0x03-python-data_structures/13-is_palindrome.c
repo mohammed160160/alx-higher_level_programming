4b10acec1d5fa4568c3ba1a3fb31fc0b3e91dd97
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-05 13:56:35 +0000

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,7 +10,7 @@
 
 int is_palindrome(listint_t **head)
 {
-int e = 0, dif = 0, middle = 0, length = 0, x = 0;
+int e = 0, dif = 0, middle = 0, length = 0;
 listint_t *start, *lengthfinder;
 
 if (head == NULL || *head == NULL) /*if the list is empty*/
@@ -36,14 +36,6 @@ for (e = 0; e < length; e++)
 array[e] = start->n;
 start = start->next;
 }
-
-
-while (array[x] != '\0')
-{
-printf("---------%d\n",array[x]);
-x++;
-}	
-
 
 middle = length / 2;
 
