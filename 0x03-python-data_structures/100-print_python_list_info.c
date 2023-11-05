1d5a1170693bb680fd7d71f57b14f2aa2a5d8fdf
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-05 15:08:03 +0000

    task 100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - print python inputs in c
+ * @p: OyObject list
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size, space, x;
+	PyObject *obj;
+
+	size = Py_SIZE(p);
+	space = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = 8\n", space);
+	for (x = 0; x < size; x++)
+	{
+	printf("Element %d: ", x);
+
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", PyTYPE(obj)->tp_name);
+	}
+}
