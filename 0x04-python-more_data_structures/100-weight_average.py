d7c5d028e72dfe44f4d4cd03250d9688622b752b
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-08 18:13:58 +0000

    task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not(my_list):
+    if not my_list:
         return (0)
-    top = 0;
-    bottom = 0;
+    top = 0
+    bottom = 0
     for group in my_list:
         top += group[0] * group[1]
         bottom += group[1]
