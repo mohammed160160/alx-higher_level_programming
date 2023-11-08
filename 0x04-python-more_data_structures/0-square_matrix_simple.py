65f95b7357c9163a6dda919a25039f7218b428d2
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-08 14:41:46 +0000

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,23 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    print(matrix)
-
-    listnum = len(matrix)
-    listelement = len(matrix[0])
-     
-    print("======{:d} listnum".format(listnum))
-    print("======{:d} listelement".format(listelement))
-
-    mat = [[None] * listelement] * listnum
-
-    print(mat)
-
-    for i in range(0, listnum):
-
-        for j in range(0, listelement):
-            mat[i][j]
-            print(mat)
-            print("======{:d} i {:d} j========".format(i, j))
-
-            mat[i][j] = (mat[i][j]) ** 2
-    return (mat)
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
