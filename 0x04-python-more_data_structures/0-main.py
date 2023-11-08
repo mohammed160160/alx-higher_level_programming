ecb5137e81f4187466cf4a98f16bce6172a7f965
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-08 11:08:22 +0000

    task 1

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -4,7 +4,8 @@ square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
-    [7, 8, 9]
+    [7, 8, 9],
+    [10, 11, 12]
 ]
 
 new_matrix = square_matrix_simple(matrix)
---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+def search_replace(my_list, search, replace):
+    new_list = list(my_list)
+    for i in range(0, len(new_list)):
+        if (new_list[i] == search):
+            new_list[i] = replace
+    return new_list
