6e5599086c7228c34f85a92399c18a541f047187
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-20 14:42:36 +0000

    task 33

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,8 @@ def safe_print_division(a, b):
     try:
         value = x / y
     except ZeroDivisionError:
+        print("Inside result: None")
         return (None)
     finally:
-
+        print("Inside result: {}".format(value))
         return (value)
