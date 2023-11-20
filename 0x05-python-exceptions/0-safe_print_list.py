4e2cc84b7acb5b45d76976b2973b6274d3fe9581
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-20 12:47:31 +0000

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,6 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end = '')
             printed += 1
-        except IndexError:
+        except (IndexError, TypeError):
             break
-    print("\n")
     return(printed)
