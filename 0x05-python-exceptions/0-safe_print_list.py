b4a0b55e299c847ff9bac64f87413a2431048efd
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-20 12:58:38 +0000

    task 00000

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     printed = 0
-    for i in range (x):
+    for i in range(x):
         try:
-            print(my_list[i], end = '')
+            print(my_list[i], end='')
             printed += 1
         except (IndexError, TypeError):
             break
-    print(end = '\n')
-    return(printed)
+    print(end='\n')
+    return (printed)
