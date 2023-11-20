aace37239af793737ded09287fa1522ec79dcc86
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-20 17:20:47 +0000

    task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except Exception as e:
+        print(f"Exception: {e}", file=sys.stderr)
+        return (False)
