89fd231178ad6b40a55d8cad9273ec95e6139e98
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-21 13:14:38 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+class Square:
+    """This is a square"""
+
+    def __init__(self, size = 0):
+        """"A construction with private size as argument."""
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer') 
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.size = size
