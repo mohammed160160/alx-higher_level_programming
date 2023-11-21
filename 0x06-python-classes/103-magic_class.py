08acea2c4a7849f07dd32f0a934d7ddfabd0f642
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-21 18:48:05 +0000

    task 103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """"Magical class"""
 
+import math
+
 
 class Magicclass:
-    """This is a magic class that takes a float or int as radius and
-    then use it's area and circumference in 2 different constructions"""
-
-
-    def __init__(self, radius):
+    """Takes a float or int as radius and then use it's area and circumference
+    in 2 different constructions"""
+    def __init__(self, radius=0):
         if ((not isinstance(radius, int)) and (not isinstance(radius, float))):
             raise TypeError('radius must be a number')
         self.__radius = radius
 
 
-
+    """This is used to find the area"""
     def area(self):
         return (math.pi * (self.__radius ** 2))
-   
 
+    """This is used to find the circumference"""
     def circumference(self):
         return (2 * self.__radius * math.pi)
