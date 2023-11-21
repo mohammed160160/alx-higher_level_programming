efa9d0ab77a47d58abb41a72ad5e9f360b4df253
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-21 17:10:41 +0000

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+""""Square module"""
+
+
+class Square:
+    """This is a square with size"""
+
+    def __init__(self, size=0):
+        """"A construction with size as argument"""
+        self.size = size
+
+    @property
+    def size(self):
+        """The properities of the size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """To check for the type and value and raise if either is false"""
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """"A Construction for the area of the square"""
+        return (self.__size ** 2)
