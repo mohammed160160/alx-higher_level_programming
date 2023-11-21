c39408c2aca733a4350319d66e0babbbf51c29e4
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-21 13:17:59 +0000

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+class Square:
+    """This is a square"""
+
+    def __init__(self, size = 0):
+        """"A construction with private size as argument."""
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """"A Construction for the area of the square"""
+        return(self.__size ** 2)
