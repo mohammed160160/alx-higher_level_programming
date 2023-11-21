5179f7c2210a20bab0099d558f1e9c9159bc3305
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-21 13:58:35 +0000

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-class Square:
-    """This is a square"""
+""""Square module"""
 
-    def __init__(self, size = 0):
-        """"A construction with private size as argument."""
+
+class Square:
+    """This is a square with size"""
+
+    def __init__(self, size=0):
+        """"A construction with private size as argument and raising error
+        in 2 situations first if the type of size isnt an integer and second
+        if the value of size is less than 0"""
 
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
@@ -13,4 +18,4 @@ class Square:
 
     def area(self):
         """"A Construction for the area of the square"""
-        return(self.__size ** 2)
+        return (self.__size ** 2)
