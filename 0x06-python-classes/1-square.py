f9d212541ba12e1d2d87437299f45b1b4db8f692
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-21 12:50:28 +0000

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """This is a square"""
+
+    def __init__(self, size):
+        """"A construction with private size as argument."""
+        self.__size = name
