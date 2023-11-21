4c7e4d8ef094cee3fc2ad6f45ddf66a349f1d158
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-21 13:44:51 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,7 +3,9 @@ class Square:
     """This is a square"""
 
     def __init__(self, size = 0):
-        """"A construction with private size as argument."""
+        """"A construction with private size as argument and raising error
+        in 2 situations first if the type of size isnt an integer and second
+        if the value of size is less than 0"""
 
         if not isinstance(size, int):
             raise TypeError('size must be an integer') 
