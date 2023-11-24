82707878b811e95c68e40e91900fb647735297f2
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-24 17:10:25 +0000

    tasks

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
+    if (not(isinstance(div, int)) and not(isinstance(div, float))):
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise TypeError('division by zero')
+    
+    listnumber = len(matrix)
+    for i in range(0, listnumber):
+        listelements = len(matrix[i])
+        
+
+        for j in range(0, listelements):
+            
+        print(end='\n')
