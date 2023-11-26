71b277458ddc2a55c7c73aeee33cf3224bae242d
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-26 09:23:37 +0000

    task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""Say_my_name"""
+
+
 def say_my_name(first_name, last_name=""):
-    if not (isinstance(first_name, str):
+    """A function that takes a string (first_name and last_name)
+    as it's arguments"""
+    if not isinstance(first_name, str):
             raise TypeError('first_name must be a string')
-    if not (isinstance(last_name, str):
+    if not isinstance(last_name, str):
             raise TypeError('last_name must be a string')
-    print("My name is {} {}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
