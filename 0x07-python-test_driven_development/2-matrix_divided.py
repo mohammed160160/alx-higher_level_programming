7843aa585398c18be4ad435e6ede99101ebef74a
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-27 09:13:36 +0000

    task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,11 +15,7 @@ def matrix_divided(matrix, div):
             raise TypeError('Each row of the matrix must have the same size')
         value = len(matrix[i])
         for j in range(0, value):
-            print(matrix[i][j])
-            if (not(isinstance(matrix[i][j], int)) and not(isinstance(matrix[i][j], float))):
+            if not isinstance(matrix[i][j], (int, float)):
                 raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
 
-    for i in range(0, len(matrix)):
-        for j in range(0, value):
-            divided[i][j] = round(divided[i][j] / div, 2)
-    return(divided)
+    return ([[round(x / div, 2) for x in row] for row in matrix])
