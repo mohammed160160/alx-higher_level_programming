8334084655ab973e8830f59e43704f74bfa38275
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-27 10:23:41 +0000

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,7 +1,40 @@
 #!/usr/bin/python3
-"""Square Module"""
+"""Rectangle Module with length and width"""
 
 
-class Square():
-    """This is a square"""
-    pass
+class Rectangle():
+    """This is a Rectangle with 2 integers representing length and width"""
+    def __init__(self, width=0, height=0):
+        """A starting construction of the class"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Sets the width as a private attribute"""
+        return (self.__width)
+
+    @property
+    def height(self):
+        """Sets the height as a private attribute"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """To check that height is an int value that is not less than 0
+        if not raise error"""
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        """To check that width is an int value that is not less than 0
+        if not raise error"""
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__width = value
