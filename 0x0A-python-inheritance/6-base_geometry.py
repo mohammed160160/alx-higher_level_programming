8f53ca1c56656a0ffde5ba69c8577f46cb423846
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-04 11:08:12 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""BaseGeometry Module"""
+
+
+class BaseGeometry():
+    """This is a BaseGeometry"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception ("area() is not implemented")
