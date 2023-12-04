a31fa9f85e1259d86018a08e8c4ab2469a48a82c
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-04 12:51:02 +0000

    task 7

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""BaseGeometry Module"""
+
+
+class BaseGeometry():
+    """This is a BaseGeometry"""
+
+    def area(self):
+        """Area is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Checks if value is a postive integer"""
+        if type(value) != int:
+            raise Exception("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise Exception("{:s} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """This is an class that inheret from Base Geometry"""
+    
+    def __init__(self, width, height):
+        """Creates a rectangle with width and height validated"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
