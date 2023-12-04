d43c3af824fa1a2aa0fe75f73ff8b1a227ce394d
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-04 11:57:09 +0000

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,5 +7,4 @@ def inherits_from(obj, a_class):
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return (True)
     else:
-        return (False)
-~                          
+        return (False) 
