450a368ec754768462da8a994ce17971eb5e0ddf
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-05 13:49:34 +0000

    task 8

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ saving = __import__('5-save_to_json_file').save_to_json_file
 loading = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    argument = loading("add_item.json") 
+    argument = loading("add_item.json")
 except FileNotFoundError:
     argument = []
 
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""save_to_json_file"""
+
+import json
+
+
+def class_to_json(obj):
+    """Return the dictionary represntation of json for the object"""
+    return (obj.__dict__)
