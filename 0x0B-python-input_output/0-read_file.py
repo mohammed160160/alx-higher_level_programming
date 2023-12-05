7fbd578da3684ef6ce7447c9845bb25e9412bea1
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-05 08:50:42 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Reading from a file"""
+
+
+def read_file(filename=""):
+    """Reading a file"""
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end='')
