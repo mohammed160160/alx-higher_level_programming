e779963d7ac6827b2a7c0cdab314fa832f9c587d
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-05 12:26:23 +0000

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""save_to_json_file"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """saves the JSON representation into a file"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
