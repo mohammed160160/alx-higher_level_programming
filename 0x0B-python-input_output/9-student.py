0e2277d69d0e0e5ebbb5b7d9d7b80c305e2b874a
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-05 13:57:41 +0000

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Student to Json"""
+
+
+class Student():
+    """A Class of student containing firstname, lastname
+    and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """To initalize the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return the dictionary represntation of json for the student class"""
+        return (self.__dict__)
