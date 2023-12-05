6ec41493ac548ea7076117a59975e5e43c150386
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-05 18:51:51 +0000

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""This is a program to write a list of list in pyramid"""
+
+
+def pascal_triangle(n):
+    """A function for a triangle"""
+    triangle = []
+
+    if n <= 0:
+        return (triangle)
+
+    linenum = 0
+
+    while len(triangle) != n:
+        line = []
+        lastline = triangle[linenum - 1]
+        linenum += 1
+        for i in range(linenum):
+            line.append(i+1)
+        triangle.append(line)
+
+    return (triangle)
