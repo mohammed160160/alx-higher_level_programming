fafbe1e440ffa21801f8a0f0978d67304aea30da
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-05 09:24:25 +0000

    tasks

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Writing into a file"""
+
+
+def write_file(filename="", text=""):
+    """Reading a file"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        return (f.write(text))
