cd58b5ee5995157299db7248f0a86fce5f6a64be
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-05 07:37:40 +0000

    task 7

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,6 +9,7 @@ class MyList(list):
         """prints a list of int in order from big to small"""
         print(sorted(self))
 
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/1-my_list.txt")
