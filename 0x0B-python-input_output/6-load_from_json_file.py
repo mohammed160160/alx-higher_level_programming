61442408dd593ac08af1bce8cc99156259ccc682
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-05 12:30:14 +0000

    task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""save_to_json_file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a JSON file"""
+    with open(filename, encoding='utf-8') as f:
+        return (json.load(f))
