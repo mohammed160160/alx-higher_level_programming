51c19cfbe913a1e5ce82b34101282502aedeb65f
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-05 15:39:35 +0000

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Student to Json"""
+
+
+class Student():
+    """A Class of student containing firstname, lastname
+    and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """To initalize the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return the dictionary represntation of json for the student class
+        but if attrs is a list of string return the attributes in list"""
+        if type(attrs) is list:
+            if ((type(element) is str) for element in attrs):
+                return {x: getattr(self, x) for x in attrs if getattr(self, x)}
+        return (self.__dict__)
