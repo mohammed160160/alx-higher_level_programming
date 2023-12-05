1cddded7070342dd4705d2a835d42fa4b18916e8
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-05 13:43:31 +0000

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""adding an argument to a JSON file"""
+
+import sys
+
+
+saving = __import__('5-save_to_json_file').save_to_json_file
+loading = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    argument = loading("add_item.json") 
+except FileNotFoundError:
+    argument = []
+
+argument.extend(sys.argv[1:])
+saving(argument, "add_item.json")
