c78f2276d09e789a5ff44f77efd19a85893cbcac
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-08 10:57:21 +0000

    task 0

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""""The base of the class system"""
+
+
+class Base():
+    """The base class for this project"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """The id system for the program"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
