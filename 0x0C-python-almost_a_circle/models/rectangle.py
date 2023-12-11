a99890566c4aaa487351c0b5b2bad1c01b6c2e9e
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-12-11 15:11:58 +0000

    task 5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,7 +75,7 @@ class Rectangle(Base):
     def __str__(self):
         """Returns a representation of the rectangle in text"""
         Rect = ""
-        Rect += "[Rectangle] ({})".format(self.id)
+        Rect += "[Rectangle] ({}) ".format(self.id)
         Rect += "{}/{} - ".format(self.__x, self.__y)
         Rect += "{}/{}".format(self.__width, self.__height)
         return (Rect)
