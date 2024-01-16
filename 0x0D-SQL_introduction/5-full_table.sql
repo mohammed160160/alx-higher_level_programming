1eb39c7d2b88e979fe49b7f97a527ee17bc69513
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-01-16 09:49:18 -0800

    tasks

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- Show full description of the table first_table in the MySQL server.
+SHOW CREATE TABLE `first_table`;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- List all rows of the table first_table in the MySQL server.
+SELECT * FROM `first_table`;
