facc7ed5df320ff011c31d45f8dab367e6c230f4
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-01-18 09:05:40 -0800

    tasks

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- List the Max temperature in each state
-SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state DESC;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
