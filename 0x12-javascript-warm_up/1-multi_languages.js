362434e43aa52a5242aa98cdb14bb449c5504c08
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-02-12 08:05:19 +0000

    tasks

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const VarC = 'C is fun';
+const VarPython = 'Python is cool';
+const VarJava = 'JavaScript is amazing';
+console.log(VarC);
+console.log(VarPython);
+console.log(VarJava);
