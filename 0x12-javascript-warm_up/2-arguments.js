3945dacdc2812d79adfe9096c049feadfa96e9cf
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-02-12 09:37:08 +0000

    tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const arg = process.argv.length - 1;
+
+if (arg < 2) {
+  console.log('No argument');
+} else if (arg === 2) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
