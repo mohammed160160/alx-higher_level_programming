97a10cbede2260a47c8d2eb96b23004a7cd56c83
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-02-12 10:30:07 +0000

    tasks

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+const Value = parseInt(process.argv[2]);
+
+if (isNaN(Value)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${Value}`);
+}
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+let i;
+const Value = parseInt(process.argv[2]);
+const loop = 'C is fun';
+
+if (isNaN(Value)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (i = 0; i < Value; i++) {
+    console.log(loop);
+  }
+}
