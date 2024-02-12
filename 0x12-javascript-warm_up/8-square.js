9b0d3fd2f1c2bc4be496e6ecabfc25912f1ca9ad
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-02-12 10:53:18 +0000

    tasks

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+let x;
+let y;
+const Value = parseInt(process.argv[2]);
+
+if (isNaN(Value)) {
+  console.log('Missing size');
+} else {
+  for (x = 0; x < Value; x++) {
+    let line = '';
+    for (y = 0; y < Value; y++) {
+      line += 'X';
+    }
+    console.log(line);
+  }
+}
