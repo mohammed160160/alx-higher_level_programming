28e72b59b1307c89fe3f2d7bf7f791d679339d56
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-02-12 13:58:42 +0000

    tasks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,8 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const argument = (process.argv.slice(2)).map(Number);
+  const list = process.argv.slice(2);
+  const argument = list.map(Number);
   const sortedargument = argument.sort((a, b) => b - a);
   console.log(sortedargument[1]);
 }
