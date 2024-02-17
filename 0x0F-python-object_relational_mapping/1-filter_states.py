b81a012d9c080855728bc28023106b7ef1245e98
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-02-17 10:56:57 +0000

    tasks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states "
-                   "WHERE name LIKE 'N%' "
+                   "WHERE name LIKE BINARY 'N%' "
                    "ORDER BY states.id")
 
     info = cursor.fetchall()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
+"""Lists all states that start with name matches the argv4"""
 import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states "
+                   "WHERE name LIKE BINARY '{}' "
+                   "ORDER BY states.id".format(sys.argv[4]))
+
+    info = cursor.fetchall()
+
+    for data in info:
+        print(data)
+
+    cursor.close()
+    db.close()
