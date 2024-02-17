ad31f9c69b49e8562b20d3d50851bf08357562e0
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-02-17 11:17:01 +0000

    tasks

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
+"""Lists all states with name matches the argv4 protected for SQL injection"""
 import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+
+    cursor = db.cursor()
+    matching_name = sys.argv[4]
+    cursor.execute("SELECT * FROM states "
+                   "WHERE name LIKE %s "
+                   "ORDER BY states.id", (matching_name,))
+
+    info = cursor.fetchall()
+
+    for data in info:
+        print(data)
+
+    cursor.close()
+    db.close()
