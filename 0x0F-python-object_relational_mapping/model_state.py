63337e0c33540a9dc814acbde8e2b019127d4aeb
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-02-18 11:43:23 +0000

    tasks

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-""""""
-import sys
-from model_state import Base, State
-from sqlalchemy import (Create_engine)
+"""Creates a Class Base and State to use"""
 
-if __name__ == "__main__":
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """A Creator for the base State"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
