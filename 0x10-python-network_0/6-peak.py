802dbd857a5adc77a006fa7298064f0bd0beffb0
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-02-29 11:19:44 +0000

    tasks

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,3 +3,18 @@
 
 def find_peak(list_of_integers):
     """This is the function"""
+
+    if len(list_of_integers) == 0:
+        return
+
+    leftside = 0
+    rightside = len(list_of_integers) - 1
+
+    while leftside < rightside:
+        middle = (leftside + rightside) // 2
+        if list_of_integers[middle] < list_of_integers[middle + 1]:
+            leftside = middle +1
+        else:
+            rightside = middle
+
+    return list_of_integers[leftside]
