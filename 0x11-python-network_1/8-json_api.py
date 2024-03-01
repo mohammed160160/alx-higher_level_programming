8b11fb94961f00a4c4a9aea62b846cf4a2373920
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-03-01 14:39:29 +0000

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
-import urllib
-'This is a description'\n\n
-if __name__ == __main__:
+"""This is a description"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    VarQ = {'q': sys.argv[1]}
+    REQUEST = requests.post(url, VarQ)
+    
+    try:
+        JASON = REQUEST.json()
+        if JASON:
+            print("[{}] {}".format(JASON['id'], JASON['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
