b8c1f6f711d91384bf910cd6aaf55b379a2bac33
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-03-01 13:33:44 +0000

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-import urllib
-'This is a description'\n\n
-if __name__ == __main__:
+"""pulls info from https://alx-intranet.hbtn.io/status using Request"""
+import requests
+
+if __name__ == "__main__":
+    REQUEST = requests.get('https://alx-intranet.hbtn.io/status').text
+    print("Body response:")
+    print("\t- type: {}".format(type(REQUEST)))
+    print("\t- content: {}".format(REQUEST))
