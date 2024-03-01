8bcef949150a884d199997d458a910cde0356335
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-03-01 10:27:29 +0000

    task 0/1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-import urllib
+import urllib.request
 """This is a description"""
 
 
 if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as opener:
+        info = opener.read()
+    print("Body response:")
+    print("\t- type:", type(info))
+    print("\t- content:", info)
+    print("\t- utf8 content:", info.decode('utf-8'))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-import urllib
+import urllib.request
+import sys
 """This is a description"""
 
 
 if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as opener:
+        print(opener.headers.get('X-Request-Id'))
