ad5c2e33a585b070c7bfd58e883660060bde0347
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-03-01 13:29:28 +0000

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-import urllib
-'This is a description'\n\n
-if __name__ == __main__:
+"""This is a description"""
+import urllib.parse
+import urllib.request
+from urllib.error import HTTPError
+import sys
+
+
+if __name__ == "__main__":
+    REQUEST = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(REQUEST) as Opened:
+            print(Opened.read().decode('utf-8'))
+    except HTTPError as err:
+        print("Error code: {}".format(err.code))
