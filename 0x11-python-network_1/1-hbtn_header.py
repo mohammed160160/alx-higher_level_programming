e9198c359027b03c40e4954a313bb19294d40d66
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-03-01 11:38:16 +0000

    task 0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""takes and requests from a URL displaying value of X-Reader"""
 import urllib.request
 import sys
-"""takes and requests from a URL displaying value of X-Reader"""
 
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as opener:
