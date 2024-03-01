7006fe506460fac6a026271ade2fb41d23147957
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-03-01 13:20:10 +0000

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
-import urllib
 """This is a description"""
+import urllib.parse
+import urllib.request
+import sys
 
 
 if __name__ == "__main__":
+    URL = sys.argv[1]
+    EMAIL = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
+    REQUEST = urllib.request.Request(URL, EMAIL)
+
+    with urllib.request.urlopen(REQUEST) as Opened:
+        Message = Opened.read()
+    print(Message.decode('utf-8'))
