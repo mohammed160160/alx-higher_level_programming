61b658734ac6d4d8cc26be4ee75fec4f4b211677
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-03-01 11:06:50 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as opener:
         info = opener.read()
-        print("Body response:")
-        print("\t- type:", type(info))
-        print("\t- content:", info)
-        print("\t- utf8 content:", info.decode(encoding="utf-8"))
+    print("Body response:")
+    print("\t- type: {}".format(type(info)))
+    print("\t- content: {}".format(info))
+    print("\t- utf8 content: {}".format(info.decode(encoding="utf-8")))
