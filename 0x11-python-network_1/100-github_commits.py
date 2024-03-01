bf2d3c01d608d937020c5a494618e224bf6884cc
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-03-01 16:34:26 +0000

    task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,3 +8,9 @@ if __name__ == "__main__":
     Owner = sys.argv[2]
     URL = 'https://api.github.com/repos/{}/{}/commits'.format(Owner, Repo)
     REQUEST = requests.get(URL)
+
+    if REQUEST.status_code == 200:
+        Commit_list = REQUEST.json()
+        for commit in Commit_list[:10]:
+            author_name = commit['commit']['author']['name']
+            print("{}: {}".format(commit['sha'], author_name))
