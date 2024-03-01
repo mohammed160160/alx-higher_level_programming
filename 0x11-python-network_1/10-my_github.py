47b042bbe98be13fc09d804d9ca16e6f9f531639
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-03-01 15:12:28 +0000

    ????

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     URL = "https://api.github.com/user"
-    Checking=(sys.argv[1], sys.argv[2])
+    Checking = (sys.argv[1], sys.argv[2])
     REQUEST = requests.get(URL, auth=Checking)
 
     if REQUEST.status_code == 200:
