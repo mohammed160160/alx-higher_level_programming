cd21546bbff22803f0458cad44dd4232a022fe98
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-03-26 19:52:27 +0000

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieID = process.argv[2];
+const URL = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+request.get(URL, (error, response, body) => {
+  if (error) {
+    console.error('Error occurred:', error);
+  } else {
+    if (response.statusCode === 200){
+      const movieINFO = JSON.parse(body);
+      console.log(`Title: ${movieINFO.title}`);	
+    } else {
+      console.error('Error occurred:', error);
+    }
+  }
+});
